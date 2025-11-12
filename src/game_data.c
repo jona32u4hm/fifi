0d57b2e6cc917cf0865bd2dafaac1dbda96c7856
jona <jona32u4hm@gmail.com>
2025-11-12 11:56:32 -0600

    added a levels directory and modified level data structs

--- src/game_data.c
+++ src/game_data.c
@@ -1,28 +1,45 @@
 #include "level_data.h"
 
+
+
+
+
+
+
+
+
 static LevelData allLevels[] = {
     // Level ID 1 (Index 0)
     {
-        .mapWidth = 64, 
+        .mapWidth = 32, 
+        .mapHeigth = 32, 
         .tileMapData = NULL,
         .enemyCount = 5,
         .enemies = NULL,
-        .playerStart = {.startPos = {50, 50}, .startHealth = 100.0f}
+        .playerStart = {.startPos = {50, 50}}
         
     },
     // Level ID 2 (Index 1)
     {
-        .mapWidth = 64, 
+        .mapWidth = 32, 
+        .mapHeigth = 32, 
         .tileMapData = NULL,
         .enemyCount = 5,
         .enemies = NULL,
-        .playerStart = {.startPos = {50, 50}, .startHealth = 100.0f}
+        .playerStart = {.startPos = {50, 50}}
         
     },
     // ...
 };
-
 const int TOTAL_LEVELS = sizeof(allLevels) / sizeof(LevelData); // Calculate total number of levels
+
+
+
+
+
+
+
+
 
 // Function to fetch the level data based on the ID
 LevelData GetLevelData(int levelID) {
@@ -30,6 +47,6 @@ LevelData GetLevelData(int levelID) {
         // Arrays are 0-indexed, so Level 1 is at index 0
         return allLevels[levelID - 1]; 
     }
-    // Handle error or return a default/empty level data structure
+    // default empty level data structure
     return (LevelData){0}; 
 }
