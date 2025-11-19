c31e3b4abed6dc61153cab94d734a56a13b4db47
jona <jona32u4hm@gmail.com>
2025-11-19 11:42:21 -0600

    pantalla ajustada a dimensiones tamién

--- src/player.c
+++ src/player.c
@@ -1,6 +1,10 @@
 #include <raylib.h>
 #include <entities.h>
-
+// --- PIXEL ART ENGINE CONFIGURATION ---
+#define GAME_WIDTH 160
+#define GAME_HEIGHT 144
+#define PIXEL_SCALE 5 // The fixed multiplier for rendering (e.g., 5X scale)
+// --------------------------------------
 //move_player changes player position, direction and current texture
 void move_player(Entity* player, Camera2D* camera) {
     if (IsKeyDown(KEY_W)){
@@ -53,8 +57,16 @@ void move_alien(Entity* alien) {
 //Initializing main
 int main(void) {
 
-    InitWindow(600, 400, "Main window");
+	const int screenWidth = GAME_WIDTH * PIXEL_SCALE;
+    const int screenHeight = GAME_HEIGHT * PIXEL_SCALE;
+   
+    InitWindow(screenWidth, screenHeight, "FINDING FIFI");
     SetTargetFPS(30);
+	
+	RenderTexture2D target = LoadRenderTexture(GAME_WIDTH, GAME_HEIGHT);	//la pantalla virtual de baja resolución
+    SetTextureFilter(target.texture, TEXTURE_FILTER_POINT);    // para evitar que se vea borroso
+    
+    
     Camera2D camera = {0};
     camera.target = (Vector2){0.0f, 0.0f};
     camera.offset = (Vector2){0.0f, 0.0f};
@@ -82,8 +94,8 @@ int main(void) {
         .current_texture = player.textures.texture_down,
         .hp = 100,
         .dest_rect = (Rectangle){
-        .x = 100,
-        .y = 200,
+        .x = 10,
+        .y = 20,
         .width = 16,
         .height = 16,
         }
@@ -101,8 +113,8 @@ int main(void) {
         .current_texture = alien.textures.texture_down,
         .hp = 100,
         .dest_rect = (Rectangle){
-        .x = 400,
-        .y = 200,
+        .x = 40,
+        .y = 20,
         .width = 16,
         .height = 16,
         }
@@ -111,7 +123,7 @@ int main(void) {
 
     //Opening window
     while(!WindowShouldClose()) {
-        BeginDrawing();
+        BeginTextureMode(target);
             ClearBackground(RAYWHITE);
 	    BeginMode2D(camera);
             if (player.hp > 0) {
@@ -124,6 +136,32 @@ int main(void) {
                 DrawTexturePro(alien.current_texture, (Rectangle){0, 0, alien.direction * 16, 16}, alien.dest_rect, (Vector2){0, 0}, 0.0, RAYWHITE);
 }
 	    EndMode2D();
+        EndTextureMode();
+        
+        BeginDrawing();
+            ClearBackground(BLACK); 
+            
+			Rectangle sourceRect = { 
+				0.0f, 
+				0.0f, 
+				(float)target.texture.width, 
+				(float)-target.texture.height // FLIP!
+			};
+			Rectangle destRect = { 
+				0.0f, // top-left 
+				0.0f, // top-left 
+				(float)GAME_WIDTH * PIXEL_SCALE,  
+				(float)GAME_HEIGHT * PIXEL_SCALE  
+			};
+			DrawTexturePro(
+				target.texture, 
+				sourceRect, 
+				destRect, 
+				(Vector2){ 0.0f, 0.0f }, // draw from top-left
+				0.0f,                    // Rotation
+				WHITE                    // Tint
+			);
+            
         EndDrawing();
     };
 
