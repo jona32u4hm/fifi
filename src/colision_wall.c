51182a421a37b57d3352ee2108d1590653eb637a
sebas-araya <arayafukssebastian@gmail.com>
2025-11-20 19:57:22 -0600

    colisiones.h para el mapa, es decir las paredes

---
+++ src/colision_wall.c
@@ -0,0 +1,37 @@
+#include "colision_wall.h"
+#include <stdio.h>
+
+#define TILE_SIZE 16
+
+int CheckMapCollision(LevelData *level, float px, float py) {
+
+    int tileX = (int)(px / TILE_SIZE);
+    int tileY = (int)(py / TILE_SIZE);
+
+    // verifica límites
+    if (tileX < 0 || tileX >= level->mapWidth ||
+        tileY < 0 || tileY >= level->mapHeight) {
+        return 1; // fuera del mapa = colisión
+    }
+
+    byte tile = level->map[tileY][tileX];
+
+    return (tile == 1);
+}
+
+/
+int CheckPlayerCollision(LevelData *level, float playerX, float playerY,
+                         float playerHalfWidth, float playerHalfHeight)
+{
+    float left   = playerX - playerHalfWidth;
+    float right  = playerX + playerHalfWidth;
+    float top    = playerY - playerHalfHeight;
+    float bottom = playerY + playerHalfHeight;
+
+    if (CheckMapCollision(level, left, top))    return 1;
+    if (CheckMapCollision(level, right, top))   return 1;
+    if (CheckMapCollision(level, left, bottom)) return 1;
+    if (CheckMapCollision(level, right, bottom))return 1;
+
+    return 0;
+}
