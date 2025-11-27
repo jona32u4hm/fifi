48aa14557a1505133bc3e685fbb94701a8fb464a
sebas-araya <arayafukssebastian@gmail.com>
2025-11-26 20:59:06 -0600

    replace old collision algorithm with this one

--- include/colisions.h
+++ include/colisions.h
@@ -1,1 +1,10 @@
+#ifndef COLISIONS_H
+#define COLISIONS_H
 
+#include "entities.h"
+#include <raylib.h>
+#include <stdbool.h>
+
+bool CheckPlayerEnemyCollision(Entity *player, Entity *enemy);
+
+#endif
