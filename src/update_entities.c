577ff2fc7e37bd4dc5572351f239c914f9bbc16b
sebas-araya <arayafukssebastian@gmail.com>
2025-11-27 11:15:25 -0600

    forgot to add patrol_alien to void function

--- src/update_entities.c
+++ src/update_entities.c
@@ -19,7 +19,7 @@ Camera2D initialize_camera() {
 
 
 
-void update_entities(Camera2D* camera, Entity* player, Entity* alien, Entity* wuwa_the_alien, Entity* melee, entity_array* proj_array, float* cooldown_proj, float* cooldown_melee, LevelData* currentLevel) {
+void update_entities(Camera2D* camera, Entity* player, Entity* alien, Entity* wuwa_the_alien, Entity* patrol_alien, Entity* melee, entity_array* proj_array, float* cooldown_proj, float* cooldown_melee, LevelData* currentLevel) {
         //Cooldown should charge over time if not full
         if (*cooldown_proj < MIN_TIME_PROJ) {
             *cooldown_proj += GetFrameTime();
